e871bc4cea4cf628d773a1441cda35a19a940ffb
Gabriel Poma <gabriel.poma@vet-alfort.fr>
2017-04-28 15:44:20 +0200

    [WIP] Ajout option paiement en 3x

--- validation_paiement.php
+++ validation_paiement.php
@@ -10,24 +10,23 @@ require_once __DIR__ . '/api/persistence/factories/factAchat.class.php';
 require_once __DIR__ . '/api/persistence/factories/factClient.class.php';
 
 $args = array(
-		'Mt' => FILTER_DEFAULT,
-		'Ref' => FILTER_DEFAULT,
-		'Auto' => FILTER_DEFAULT,
-		'Reponse' => FILTER_DEFAULT,
-		'Sign' => FILTER_DEFAULT
-	     );
+	'Mt' => FILTER_DEFAULT,
+	'Ref' => FILTER_DEFAULT,
+	'Auto' => FILTER_DEFAULT,
+	'Reponse' => FILTER_DEFAULT,
+	'Sign' => FILTER_DEFAULT
+);
 
 $GET = filter_input_array(INPUT_GET, $args);
 
-if($GET['Auto'] === NULL) {
+if ($GET['Auto'] === NULL) {
 	utils::log(LOG_FILE, 'REF: ' . $GET['Ref'] . ' | N° Autorisation nl. Paiement refuse.');
-	$valeurs = "Mt=".$GET['Mt']."&Ref=".$GET['Ref']."&Reponse=".$GET['Reponse'];
-}
-else {
-	$valeurs = "Mt=".$GET['Mt']."&Ref=".$GET['Ref']."&Auto=".$GET['Auto']."&Reponse=".$GET['Reponse'];
+	$valeurs = "Mt=" . $GET['Mt'] . "&Ref=" . $GET['Ref'] . "&Reponse=" . $GET['Reponse'];
+} else {
+	$valeurs = "Mt=" . $GET['Mt'] . "&Ref=" . $GET['Ref'] . "&Auto=" . $GET['Auto'] . "&Reponse=" . $GET['Reponse'];
 }
 
-if($GET['Sign'] === NULL || !utils::verify_sign($valeurs, base64_decode($GET['Sign']))) {
+if ($GET['Sign'] === NULL || !utils::verify_sign($valeurs, base64_decode($GET['Sign']))) {
 	utils::log(LOG_FILE, 'REF: ' . $GET['Ref'] . ' | Signature non valide : ' . $GET['Sign']);
 	exit;
 }
@@ -44,51 +43,54 @@ $achat = factAchat::getAchatByPayement($refPaiement->getKey());
 $client = $achat->getClient();
 $produit = $achat->getProduit();
 
-if($produit->getMontantEnCentime() != $GET['Mt']) {
+/*
+ * Désactivation en attendant de savoir si l'IPN est appelée plusieurs fois ou non
+ *
+if ($produit->getMontantEnCentime() != $GET['Mt']) {
 	utils::log(LOG_FILE, 'REF: ' . $GET['Ref'] . ' | Le montant n\'est pas bon');
 	exit;
 }
-
+*/
 
 $data = array(
-                'user' => $client->getPrenom() . ' ' . $client->getNom(),
-                'reference' => $refPaiement->getReference(),
-                'produit' => $produit->getLibelle(),
-                'montant' => $GET['Mt']/100,
-                'autorisation' => $GET['Auto'],
-		'message_retour' => $message_retour,
+	'user' => $client->getPrenom() . ' ' . $client->getNom(),
+	'reference' => $refPaiement->getReference(),
+	'produit' => $produit->getLibelle(),
+	'montant' => $GET['Mt'] / 100,
+	'autorisation' => $GET['Auto'],
+	'message_retour' => $message_retour,
 );
 
 switch ($GET['Reponse']) {
 	case '00000':
 		$status = 1;
-		$template = VIEW.'/mail/succes.phtml';
+		$template = VIEW . '/mail/succes.phtml';
 		$data['title'] = 'Paiement effectué';
 		break;
 	case '99999':
 		$status = 2;
-		$template = VIEW.'/mail/attente.phtml';
+		$template = VIEW . '/mail/attente.phtml';
 		$data['title'] = 'Paiement en attente';
 		break;
 	case (preg_match('/^001[0-9]{2}$/', $GET['Reponse']) ? true : false):
 		$status = 3;
-		$template = VIEW.'/mail/refuse.phtml';
+		$template = VIEW . '/mail/refuse.phtml';
 		$data['title'] = 'Paiement refusé';
 		break;
 	case (preg_match('/^000[0-9]{2}$/', $GET['Reponse']) ? true : false):
 		$status = 4;
-		$template = VIEW.'/mail/refuse.phtml';
+		$template = VIEW . '/mail/refuse.phtml';
 		$data['title'] = 'Paiement refusé';
 		break;
 	default :
 		$status = 5;
 		$message_retour = 'Message inconnu de la part de paybox';
-		$template = VIEW.'/mail/refuse.phtml';
+		$template = VIEW . '/mail/refuse.phtml';
 		$data['title'] = 'Réponse incorrect de paybox';
 		break;
 }
 
-try{
+try {
 	$refPaiement->setPStatus($status);
 	$refPaiement->setMontant($data['montant']);
 	factPayement::writePayement($refPaiement);
@@ -100,7 +102,6 @@ try{
 		$template,
 		$data
 	);
-}
-catch (Exception $e) {
-	utils::log(LOG_FILE, 'REF: ' . $GET['Ref'] . ' REPONSE: ' . $GET['Reponse'] . ' ' . $message_retour.'|' . $e->getMessage());
+} catch (Exception $e) {
+	utils::log(LOG_FILE, 'REF: ' . $GET['Ref'] . ' REPONSE: ' . $GET['Reponse'] . ' ' . $message_retour . '|' . $e->getMessage());
 }
