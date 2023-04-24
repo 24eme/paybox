ccf49a1d2081d4abfaff51acc5527c961e5d06bf
Gabriel Poma <gpoma@24eme.fr>
2023-04-24 17:50:32 +0200

    from header

--- app/Utils.php
+++ app/Utils.php
@@ -117,7 +117,7 @@ class Utils
         $headers = array();
         $headers[] = "MIME-Version: 1.0";
         $headers[] = "Content-type: text/html; charset=utf-8";
-        $headers[] = "***REMOVED***";
+        $headers[] = "From: ***REMOVED***";
         $headers[] = "Subject: {$subject}";
         $headers[] = "X-Mailer: PHP/".phpversion();
 
